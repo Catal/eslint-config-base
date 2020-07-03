ef96594a4f21cfe82775bbaa0958fb43736d8abc
Masumi Kawasaki <701242+geeknees@users.noreply.github.com>
2020-07-03 21:13:11 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@
 const assert = require('assert')
 const eslint = require('eslint')
 
-// Use the rules defined in this repo to test against.
 const eslintOpts = {
   useEslintrc: false,
   configFile: 'index.js'
@@ -11,7 +10,6 @@ const eslintOpts = {
 
 var code = 'const foo = 1\nconst bar = (): void => (foo)\nreturn bar\n'
 
-// Runs the linter on the repo files and asserts no errors were found.
 const report = new eslint.CLIEngine(eslintOpts).executeOnText(code)
 
 assert.equal(report.errorCount, 0)
